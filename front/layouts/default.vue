4feb4c219bf0184f39ffadf70197fef5d6fa097b
Nakanisi-Kanta <71924821+Nakanisi-Kanta@users.noreply.github.com>
2020-12-10 09:34:39 +0900

    アプリ共通ディレクトリ作成

--- front/layouts/default.vue
+++ front/layouts/default.vue
@@ -1,14 +1,23 @@
 <template>
-  <v-app>
+  <v-app style="background-color: #ECEFF1;">
+    <Header />
     <v-main>
       <v-container>
         <Nuxt />
       </v-container>
     </v-main>
+    <Footer />
   </v-app>
 </template>
 
 <script>
+import Header from '../components/organisms/Header.vue'
+import Footer from '../components/organisms/Footer.vue'
+
 export default {
+  components: {
+    Header,
+    Footer
+  }
 }
 </script>
