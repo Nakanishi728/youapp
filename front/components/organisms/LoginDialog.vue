e28cf65723ffa34a61fe7d74bda61810f749c5ec
Nakanisi-Kanta <71924821+Nakanisi-Kanta@users.noreply.github.com>
2020-12-17 01:42:39 +0900

    再認証モーダル作成

---
+++ front/components/organisms/LoginDialog.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-dialog
+    v-model="dialog"
+  >
+    <v-card>
+      <p>hoge</p>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      dialog: false,
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
