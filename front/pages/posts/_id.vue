7d85cec0de887f9179d27ddb7721441a2753a98b
Nakanisi-Kanta <71924821+Nakanisi-Kanta@users.noreply.github.com>
2020-12-26 09:17:26 +0900

    投稿詳細ページ　作成

---
+++ front/pages/posts/_id.vue
@@ -0,0 +1,133 @@
+<template>
+  <div class="posts-id">
+    <ErrorAnnounce
+      :status="notFound"
+    />
+    <PostEditDialog
+      :dialog="dialog"
+      @closeDialog="dialog = false"
+    />
+    <div v-if="!notFound" class="post-show-box">
+      <v-card class="mx-auto mt-4 pa-3" width="600px">
+        <v-card-title>
+          <h2 class="post-show-title">
+            {{ post.title }}
+          </h2>
+        </v-card-title>
+        <div class="post-show-header px-2">
+          <UsersLink
+            :user="user"
+          />
+          <div
+            v-if="currentUser && currentUser.id === user.id"
+            class="post-show-change-box"
+          >
+            <v-btn
+              outlined
+              fab
+              small
+              color="light-blue lighten-3"
+              @click="dialog = !dialog"
+            >
+              <v-icon>mdi-pencil</v-icon>
+            </v-btn>
+            <v-btn
+              outlined
+              fab
+              small
+              color="red lighten-3"
+              @click="destroyPost"
+            >
+              <v-icon>mdi-delete-forever</v-icon>
+            </v-btn>
+          </div>
+        </div>
+        <LinkCard
+          v-for="link in links"
+          :key="link.id"
+          :link="link"
+        />
+      </v-card>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from '@/plugins/axios'
+import ErrorAnnounce from '~/components/molecules/ErrorAnnounce.vue'
+import LinkCard from '~/components/molecules/LinkCard.vue'
+import UsersLink from '~/components/molecules/UsersLink.vue'
+import PostEditDialog from '~/components/organisms/PostEditDialog.vue'
+
+export default {
+  components: {
+    ErrorAnnounce,
+    LinkCard,
+    UsersLink,
+    PostEditDialog
+  },
+  data () {
+    return {
+      user: {},
+      post: {},
+      point: '',
+      links: [],
+      notFound: false,
+      dialog: false
+    }
+  },
+  computed: {
+    currentUser () {
+      return this.$store.state.currentUser
+    }
+  },
+  mounted () {
+    axios
+      .get(`/v1/posts/${this.$route.params.id}`)
+      .then((res) => {
+        this.user = res.data.user
+        this.post = res.data
+        this.point = res.data.point
+        this.links = res.data.links
+      })
+      .catch((error) => {
+        if (error.response.status === 404) {
+          this.notFound = true
+        }
+      })
+  },
+  methods: {
+    openDialog () {
+      this.dialog = true
+    },
+    destroyPost () {
+      this.$store.commit('setLoading', true)
+      axios
+        .delete(`/v1/posts/${this.$route.params.id}`, { id: this.user.id })
+        .then((res) => {
+          this.$store.commit('setLoading', false)
+          this.$store.commit('setFlash', {
+            status: true,
+            message: '投稿を削除しました'
+          })
+          setTimeout(() => {
+            this.$store.commit('setFlash', {})
+          }, 2000)
+          this.$router.push('/')
+        })
+    }
+  }
+}
+</script>
+
+<style type="scoped">
+.post-point {
+  margin-top: 24px;
+}
+
+.post-show-header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+</style>
