ede612da519edda39c94ea98c2e6268474c10c1e
Nakanisi-Kanta <71924821+Nakanisi-Kanta@users.noreply.github.com>
2021-01-18 15:13:25 +0900

    herokuUp

--- api/config/environments/production.rb
+++ api/config/environments/production.rb
@@ -30,6 +30,8 @@ Rails.application.configure do
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :amazon
+  host = 'pushar.herokuapp.com'
+  Rails.application.routes.default_url_options[:host] = host
 
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
